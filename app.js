c7137b0e5355c5d5bbf1c13c00f9e55aa91f1f23
Erivan Oliveira <erivan.so@hotmail.com>
2020-10-30 21:33:27 -0300

    Método que invoca a função a cada 1 segundo

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const nextYear = new Date().getFullYear()+1;
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
 const updateCountown = () => {
+    const currentTime = new Date()
+    const difference = newYearTime - currentTime
+    const days = Math.floor(difference / 1000 / 60 / 60/ 24)
+    const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+    const minutes = Math.floor(difference / 1000 / 60 / 60) % 60
+    const secons = Math.floor(difference / 1000) % 60
 
 }
 
-updateCountown()
+setInterval(updateCountown, 1000); // a cada 1 segundo, este método vai chamar função updateCountown
 
 
