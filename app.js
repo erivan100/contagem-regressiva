013a49fbcd06e73aa8298411c95a759e9dcd7802
Erivan Oliveira <erivan.so@hotmail.com>
2020-10-30 21:41:29 -0300

    adicionado a contagem dos segundos

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const nextYear = new Date().getFullYear()+1;
+const secondsContainer = document.querySelector('#seconds')
+
+const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
 
 const updateCountown = () => {
@@ -6,8 +8,10 @@ const updateCountown = () => {
     const difference = newYearTime - currentTime
     const days = Math.floor(difference / 1000 / 60 / 60/ 24)
     const hours = Math.floor(difference / 1000 / 60 / 60) % 24
-    const minutes = Math.floor(difference / 1000 / 60 / 60) % 60
-    const secons = Math.floor(difference / 1000) % 60
+    const minutes = Math.floor(difference / 1000 / 60) % 60
+    const seconds = Math.floor(difference / 1000) % 60
+
+    secondsContainer.textContent = seconds <10 ? '0' + seconds : seconds //adicinando mais um 0
 
 }
 
