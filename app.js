e7952b93c455dbae8f323c87168cf5a9751b36e0
Erivan Oliveira <erivan.so@hotmail.com>
2020-10-30 22:24:47 -0300

    Aplicação de contagem regressiva finalizado

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
+
+const nextYearContainer = document.querySelector('#year') 
+
+
 
 const nextYear = new Date().getFullYear() + 1
 const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`)
+
+nextYearContainer.textContent = nextYear //Mstrar o proximo Ano no html.
 
 const updateCountown = () => {
     const currentTime = new Date()
@@ -12,8 +21,11 @@ const updateCountown = () => {
     const seconds = Math.floor(difference / 1000) % 60
 
     secondsContainer.textContent = seconds <10 ? '0' + seconds : seconds //adicinando mais um 0
-
+    minutesContainer.textContent = minutes <10 ? '0' + minutes : minutes
+    hoursContainer.textContent = hours <10 ? '0' + hours : hours
+    daysContainer.textContent = days <10 ? '0' + days : days
 }
+
 
 setInterval(updateCountown, 1000); // a cada 1 segundo, este método vai chamar função updateCountown
 
